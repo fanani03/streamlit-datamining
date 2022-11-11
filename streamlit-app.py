9b2547556fc859c4a7a2d664e6b975a8f92ec93a
fanani03 <afanfanani202@gmail.com>
2022-11-11 15:55:25 +0700

    Bismillah jadi

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Aplikasi Penentuan Diabetes dengan MequalityNN")
+st.title("Aplikasi Penentuan Diabetes dengan KNN")
 
 quality = st.number_input('Insert a quality')
 prescreen = st.number_input('Insert a prescreen')
@@ -22,4 +22,16 @@ euDist = st.number_input('Insert a euDist')
 diameter = st.number_input('Insert a diameter')
 amfm_class = st.number_input('Insert a amfm_class')
 
-st.button("Tentukan")
+data_baru = [[quality, prescreen, ma2, ma3, ma4, ma5, ma6, ma7, 
+                exudate8, exudate9, exudate10, exudate11, exudate12, exudate13,exudate14,exudate15,
+                euDist, diameter, amfm_class]]
+
+
+st.write(inp_pred)
+
+
+ if st.button("Tentukan"):
+    knn = joblib.load("modelKnnNormalisasi.pkl")
+    inp_pred = knn.predict(data_baru)
+    st.write(inp_pred)
+
