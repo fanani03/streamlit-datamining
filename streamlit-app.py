2e67e5528b5ff05827b4a932108be1143f8d7575
fanani03 <afanfanani202@gmail.com>
2022-11-11 14:51:16 +0700

    Penambahan semua inputan

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,3 +1,25 @@
 import streamlit as st
 
-st.title("Aplikasi Penentuan Diabetes dengan Metode KNN")
+st.title("Aplikasi Penentuan Diabetes dengan MequalityNN")
+
+quality = st.number_input('Insert a quality')
+prescreen = st.number_input('Insert a prescreen')
+ma2 = st.number_input('Insert a ma2')
+ma3 = st.number_input('Insert a ma3')
+ma4 = st.number_input('Insert a ma4')
+ma5 = st.number_input('Insert a ma5')
+ma6 = st.number_input('Insert a ma6')
+ma7 = st.number_input('Insert a ma7')
+exudate8 = st.number_input('Insert a exudate8')
+exudate9 = st.number_input('Insert a exudate9')
+exudate10 = st.number_input('Insert a exudate10')
+exudate11 = st.number_input('Insert a exudate11')
+exudate12 = st.number_input('Insert a exudate12')
+exudate13 = st.number_input('Insert a exudate13')
+exudate14 = st.number_input('Insert a exudate14')
+exudate15 = st.number_input('Insert a exudate15')
+euDist = st.number_input('Insert a euDist')
+diameter = st.number_input('Insert a diameter')
+amfm_class = st.number_input('Insert a amfm_class')
+
+st.button("Tentukan")
