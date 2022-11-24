e8f1b2c221e2cba7e6a66b7026049cc97c8c3c3e
fanani03 <afanfanani202@gmail.com>
2022-11-24 09:04:09 +0700

    datasource

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import pandas as pd               
 import numpy as np
-import seaborn as sns                       #visualisation
-import matplotlib.pyplot as plt             #visualisation
-%matplotlib inline     
-sns.set(color_codes=True)
+
 # from sklearn.neighbors import KNeighborsClassifier
 
 
