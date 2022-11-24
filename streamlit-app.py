a8b8e9dbc94c0b02528ee640da47f28e9ffccbe2
fanani03 <afanfanani202@gmail.com>
2022-11-24 13:38:17 +0700

    GaussianNB

--- streamlit-app.py
+++ streamlit-app.py
@@ -101,27 +101,27 @@ with modelling:
    model = st.radio("Silahkan memilih jenis Model",('KNN','Gaussian Naive Bayes','Decision Tree'))
 
    if model == "KNN":
-        st.header("KNN")
+        # st.header("KNN")
         
-        #Membuat k 1 sampai 25
-        k_range = range(1,26)
-        scores = {}
-        scores_list = []
-        for k in k_range:
-                knn = KNeighborsClassifier(n_neighbors=k)
-                knn.fit(xtrain,ytrain)
-                y_pred=knn.predict(xtest)
-                scores[k] = metrics.accuracy_score(ytest,y_pred)
-                scores_list.append(metrics.accuracy_score(ytest,y_pred))
+        # #Membuat k 1 sampai 25
+        # k_range = range(1,26)
+        # scores = {}
+        # scores_list = []
+        # for k in k_range:
+        #         knn = KNeighborsClassifier(n_neighbors=k)
+        #         knn.fit(xtrain,ytrain)
+        #         y_pred=knn.predict(xtest)
+        #         scores[k] = metrics.accuracy_score(ytest,y_pred)
+        #         scores_list.append(metrics.accuracy_score(ytest,y_pred))
         
-        st.write("Hasil Pengujian K=1 sampai K=25")
-        st.line_chart(pd.DataFrame(scores_list))
-        akurasi1 = accuracy_score(ytest,y_pred)
+        # st.write("Hasil Pengujian K=1 sampai K=25")
+        # st.line_chart(pd.DataFrame(scores_list))
+        # akurasi1 = accuracy_score(ytest,y_pred)
         
-        for i in range(1,25):
-            if akurasi1 == scores_list[i]:
-                k=i
-        st.success("Hasil akurasi tertinggi = " + str(akurasi1*100) + " Pada Nilai K = " + str(k))
+        # for i in range(1,25):
+        #     if akurasi1 == scores_list[i]:
+        #         k=i
+        # st.success("Hasil akurasi tertinggi = " + str(akurasi1*100) + " Pada Nilai K = " + str(k))
 
 
 
